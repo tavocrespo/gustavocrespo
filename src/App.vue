9413ab3af83b5ad94d02bbfe944e1f421311efbe
gustavocrespoc24 <gustavocrespoc24@gmail.com>
2024-05-30 13:24:34 +0000

    asas

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from 'vue';
+const gustavo = ref('');
+const crespo = ref('');
+</script>
+
+<template>
+   <div>
+    <h2><b>First Name</b></h2>
+    <br>
+    <input v-model="gustavo"  type="text" placeholder="Enter Surname" name="name" >
+    <br>
+    <h1>{{ gustavo }}</h1>
+
+    <h2><b>Surname</b></h2>
+    <br>
+    <input v-model="crespo" type="text" placeholder="Enter Surname" name="lastname">
+    <br>
+    <h1>{{ crespo }}</h1>
+   </div>
+    
+
+    
+</template>
+
+<style scoped>
+
+div{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+
+</style>
